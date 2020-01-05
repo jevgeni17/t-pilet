cffd439c3dfd30ef8a7b06ffd5bdb3473706a935
Jevgeni Barkin <zekaxdpro@gmail.com>
2020-01-05 23:26:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -36,6 +36,22 @@ def oneWay():
     departure_input = str(input("Укажите город отправления "))
     destination_input = str(input("Укажите город назначения "))
     departure_date_input = str(input("Укажите дату отправления "))
+    period_time_input = str(input("Укажите промежуток времени отправления"))
+    print("Показать вам ->")
+    choose = {
+        1: 'Самый дешёвый маршрут',
+        2: 'Все маршруты за указ.отрезок времени',
+    }
+    for key in choose:
+        print(key, '->', choose[key])
+    while True:
+        input_ = input(": ")
+        if input_ == '1':
+            cheapestTrip()     
+        elif input_ == '2':
+            roundTrip()
+        else:
+            break
     set_options(departure_input,destination_input,departure_date_input,departure_date_input)
 
 def roundTrip():
@@ -44,5 +60,8 @@ def roundTrip():
     departure_date_input = str(input("Укажите дату отправления "))
     return_date_input = str(input("Укажите дату обратного отправления "))
     set_options(departure_input,destination_input,departure_date_input,return_date_input,returningTrip='1')
+
+def cheapestTrip():
+    ticketsData('https://www.tpilet.ee/webapi/et/journeys/search?departureStop=tallinn&destinationStop=parnu&departureDate=2020-01-10&returnDate=&price=-1&duration=-1&includeConnections=false&departureBusStopId=17028&destinationBusStopId=8723')
 
 main()
